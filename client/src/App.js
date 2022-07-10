47276f8934887acd0269dae27f6ebcdef3df1332
Puneet Dabral <puneetdabral2000@gmail.com>
2022-07-11 01:17:59 +0530

    use first time fire base and also using its auth fun

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route,Routes } from 'react-router-dom';
 import { Login } from './components';
 import Home from './components/Home';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { app } from './config/firebase.config';
+import { getAuth } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  
+  const firebaseAuth =getAuth(app);
+  
+  const navigate = useNavigate();
+
+  
+  const [auth, setAuth] = useState(false || window.localStorage.getItem("auth")==="true");
+
+  useEffect(()=>{
+    firebaseAuth.onAuthStateChanged((userCred)=>{
+      if(userCred){
+        userCred.getIdToken().then((token)=>{
+          console.log(token)
+        })
+      }else{
+        setAuth(false);
+        window.localStorage.setItem("auth","false");
+        navigate('/login')
+      }
+    })
+  },[])
   return (
-    <div className="w-screen h-screen bg-primary flex justify-center items-center ">
+    <div className="flex items-center justify-center w-screen h-screen bg-primary ">
     <Routes>
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login setAuth={setAuth}/>} />
       <Route path="/*" element={<Home />} />
     </Routes>
     </div>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { app } from "../config/firebase.config";
+import {getAuth, GoogleAuthProvider , signInWithPopup} from 'firebase/auth';
+import { useNavigate } from "react-router-dom";
 
+const Login = ({setAuth}) => {
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const navigate = useNavigate();
+  const loginWithGoogle =async()=>{
+    await signInWithPopup(firebaseAuth,provider).then((userCred)=>{
+      if(userCred){
+        setAuth(true);
+        window.localStorage.setItem("auth","true");
 
-const Login = () => {
+        firebaseAuth.onAuthStateChanged((userCred)=>{
+          if(userCred){
+            userCred.getIdToken().then((token)=>{
+              console.log(token)
+            })
+
+            navigate("/",{replace:true})
+          }else{
+            setAuth(false);
+            navigate("/login")
+          }
+        })
+      }
+    })
+
+  } 
+  useEffect(()=>{
+if(window.localStorage.getItem("auth")==="true"){
+  navigate("/",{replace:true});
+}
+  },[])
   return (
     <div className="relative w-screen h-screen">
       <div className="absolute inset-0 flex items-center justify-center p-4 bg-darkOverlay">
         <div className="flex flex-col items-center justify-center w-full p-4 rounded-md shadow-2xl md:w-375 bg-light-Overlay backdrop-blur-md">
-          <div className="flex items-center justify-center gap-2 px-4 py-2 transition-all duration-100 ease-in-out rounded-md cursor-pointer bg-cardOverlay hover:bg-card hover:shadow-md">
+          <div className="flex items-center justify-center gap-2 px-4 py-2 transition-all duration-100 ease-in-out rounded-md cursor-pointer bg-cardOverlay hover:bg-card hover:shadow-md"
+          onClick={loginWithGoogle}
+          >
           <FcGoogle className="text-xl" />
             Sign in with Google
           </div>
